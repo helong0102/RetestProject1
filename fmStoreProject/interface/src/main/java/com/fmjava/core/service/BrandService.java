c5d86dfeee819c713418a99517e1ccbabe9c5e44
helong <helong0102@163.com>
2022-01-07 15:29:54 +0800

    搭建基础工程。

---
+++ fmStoreProject/interface/src/main/java/com/fmjava/core/service/BrandService.java
@@ -0,0 +1,12 @@
+package com.fmjava.core.service;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: helong
+ * @Date: 2022/01/07/15:08
+ * @Description:
+ */
+public interface BrandService {
+    public String getName();
+}
---
+++ fmStoreProject/service_sellergoods/src/main/java/com/fmjava/core/service/BrandServiceImpl.java
@@ -0,0 +1,15 @@
+package com.fmjava.core.service;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: helong
+ * @Date: 2022/01/07/15:14
+ * @Description:
+ */
+public class BrandServiceImpl implements BrandService {
+    @Override
+    public String getName() {
+        return "HeLong";
+    }
+}
